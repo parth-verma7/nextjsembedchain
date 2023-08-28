12ac7d859bf7d71fb35c7563232c4f5b79edab1b
parth-verma7 <lcb2021028@iiitl.ac.in>
2023-08-28 06:08:27 +0530

    Flask Backend Server

--- app.py
+++ app.py
@@ -1,32 +1,60 @@
-from flask import Flask, redirect, render_template, url_for, request, session
+from flask import Flask, redirect, render_template, url_for, request, session, jsonify
 import os
 from embedchain import App
-os.environ["OPENAI_API_KEY"] = "API_KEY"
-elon_bot=App()
+from flask_cors import CORS
+os.environ["OPENAI_API_KEY"] = "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"
+elon_bot = App()
 
 app = Flask(__name__)
-# app.secret_key="dsjdijbfjbvj"
+
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
+submitted_url=""
 
 @app.route('/', methods=['GET','POST'])
-def input():
-    if(request.method=='POST'):
-        doc_link=request.form.get('doc')
-        print("The link is ", doc_link)
+def submit_url():
+    if request.method == 'POST':
+        global submitted_url
+        data = request.get_json()
+        submitted_url = data.get('url')
+        print(submitted_url)
         elon_bot.add(doc_link)
-        # session['elon_bot']=elon_bot
         return redirect('/ques')
-    return render_template('index.html')
+    return "get response"
 
-@app.route('/ques', methods=['GET','POST'])
-def ques():
-    if(request.method=="GET"):
-        # elon_bot=session.get('elon_bot')
-        print("11111111111111")
-        return render_template('ques.html')
-    elif(request.method=='POST'):
-        que=request.form.get('question')
-        ans=elon_bot.query(que)
-        return ans
+@app.route('/process-text', methods=['POST'])
+def process_text():
+    global submitted_url
+    data = request.get_json()
+    text = data.get('text')
+    result = {"processedText": f"Processed: {text}"}
+    ques=result['processedText']
+    ans=elon_bot.query(ques)
+    print(ans)
+    result={'ans':ans}
+    return jsonify(result)
+
+
+# @app.route('/', methods=['GET','POST'])
+# def input():
+#     if(request.method=='POST'):
+#         doc_link=request.form.get('doc')
+#         print("The link is ", doc_link)
+#         elon_bot.add(doc_link)
+#         # session['elon_bot']=elon_bot
+#         return redirect('/ques')
+#     return render_template('index.html')
+
+# @app.route('/ques', methods=['GET','POST'])
+# def ques():
+#     if(request.method=="GET"):
+#         # elon_bot=session.get('elon_bot')
+#         print("checking")
+#         return render_template('ques.html')
+#     elif(request.method=='POST'):
+#         que=request.form.get('question')
+#         ans=elon_bot.query(que)
+#         return ans
     
 
 if __name__ == '__main__':
